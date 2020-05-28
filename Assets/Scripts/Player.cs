4a64629c22bcb843659a4869934b1f1f65224da4
1forh <1forhlol@gmail.com>
2020-05-27 21:53:41 -0400

    updates

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var x = horizontalInput * _speed * Time.deltaTime;
+        Vector3 position = new Vector3(x, 0, transform.position.z);
+        transform.Translate(position);
+    }
+}

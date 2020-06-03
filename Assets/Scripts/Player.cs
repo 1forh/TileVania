838f7caf127b8b0de9ee1d6fbe76419d387296a8
1forh <1forhlol@gmail.com>
2020-06-02 21:51:07 -0400

    updates

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,56 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _jumpHeight = 4.5f;
 
+    bool isAlive = true;
+    
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         Run();
+        Jump();
+        FlipSprite();
     }
 
     private void Run()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
-        var x = horizontalInput * _speed * Time.deltaTime;
-        Vector3 position = new Vector3(x, 0, transform.position.z);
-        transform.Translate(position);
+        var x = horizontalInput * _speed;
+        Vector2 position = new Vector2(x, _rigidBody.velocity.y);
+        _rigidBody.velocity = position;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidBody.velocity.x) > Mathf.Epsilon;
+        _animator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        var spaceInput = Input.GetKeyDown(KeyCode.Space);
+
+
+        if (spaceInput)
+        {
+            Vector2 jumpVelocity = new Vector2(0f, _jumpHeight);
+            _rigidBody.velocity += jumpVelocity;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(_rigidBody.velocity.x), 1.0f);
+        }
     }
 }
